ec8ac79bf7ef490287a2e70cc58614c693bb0f5d
janetchang <janetlaichang@gmail.com>
2013-01-09 13:17:46 -0800

    first commit, add gems, configure database for postgres

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.10'
+gem 'pg'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'annotate'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'faker'
+  gem 'rspec-rails'
+  gem 'shoulda-matchers'
+  gem 'quiet_assets'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bodydash::Application.config.secret_token = '2668a173bfacf9845b9bfbc976fd1e8914ab1691d12ff348831b7e2e192856e884aa45e011456e705578348c97258ae0f92a49a76df351bba8b5793cabf22a50'
